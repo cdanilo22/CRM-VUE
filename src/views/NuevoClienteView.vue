0caf7ed7565ce55a8c58a76c81dad6684df2d704
Danilo de Leon <cdeleonc4@miumg.edu.gt>
2024-01-02 22:11:12 -0600

    Todos los campos del formulario y validaciones fueron realizados

--- src/views/NuevoClienteView.vue
+++ src/views/NuevoClienteView.vue
@@ -23,17 +23,54 @@
              <div class="mx-auto mt-10 bg-white shadow">
                 <div class="mx-auto md:w-2/3 py-20 px-6">
                 <FormKit
-                type="form">
+                type="form"
+                submit-label="Agregar Cliente"
+                incomplete-message="No se pudo enviar, revisa los mensajes"
+                >
 
                 <FormKit
                   type="text"
                   label="Nombre"
                   placeholder="Nombre de Cliente"
-                  help="Coloca el Nombre del Cliente que deseas registrar"
                   validation="required"
                   :validation-messages="{required: 'El Nombre del Cliente es Obligatorio'}"
-                  
                   />
+                  <FormKit
+                  type="text"
+                  label="Apellido"
+                  placeholder="Nombre de Cliente"
+                  validation="required"
+                  :validation-messages="{required: 'El Apellido del Cliente es Obligatorio'}"
+                  />
+
+                  <FormKit
+                  type="email"
+                  label="Email"
+                  placeholder="Email del Cliente"
+                  validation="required|email"
+                  :validation-messages="{required: 'El Email del Cliente es Obligatorio', email: 'Coloca un email válido'}"
+                  />
+
+                  <FormKit
+                  type="text"
+                  label="Teléfono"
+                  placeholder="Teléfono: XXXX-XXXX"
+                  validation="*matches:/^[0-9]{4}-[0-9]{4}$/"
+                  :validation-messages="{matches: 'El formato no es válido'}"
+                  />
+
+                  <FormKit
+                  type="text"
+                  label="Empresa"
+                  placeholder="Empresa del Cliente"
+                  />
+
+                  <FormKit
+                  type="text"
+                  label="Puesto"
+                  placeholder="Puesto del Cliente"
+                  />
+
                 </FormKit>
               </div>
              </div>
